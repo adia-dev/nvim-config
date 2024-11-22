b2dacb55730e24df1282c59acb0ba43a136e664d
🦀 adia-dev <abdoudu78130@gmail.com>
2024-11-22 21:28:56 +0100

    Pre LSP mess

--- lua/adia-dev/lazy.lua
+++ lua/adia-dev/lazy.lua
@@ -31,7 +31,14 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { 
+    enabled = true,
+    notify = false
+  },
+
+  change_detection = {
+    notify = false,
+  },
 })
 
 require("adia-dev.plugins")
