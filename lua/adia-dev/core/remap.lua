f6e1a4d101cde4cf80878f2a02751efa2978e2dc
🦀 adia-dev <abdoudu78130@gmail.com>
2025-01-20 23:56:05 +0100

    BLUR OPAAAAAAAAAACITYYYYYYYY

--- lua/adia-dev/core/remap.lua
+++ lua/adia-dev/core/remap.lua
@@ -100,5 +100,5 @@ local function toggle_blur()
 	blur_state = not blur_state
 end
 
--- keymap.set("n", "<leader>aot", toggle_opacity, { desc = "Toggle Alacritty opacity" })
--- keymap.set("n", "<leader>abt", toggle_blur, { desc = "Toggle Alacritty blur" })
+keymap.set("n", "<leader>oT", toggle_opacity, { desc = "Toggle Alacritty opacity" })
+keymap.set("n", "<leader>oB", toggle_blur, { desc = "Toggle Alacritty blur" })
