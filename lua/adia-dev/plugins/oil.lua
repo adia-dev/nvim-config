8e428522008f292e1018c772e7cf6ef5f0e60bba
🦀 adia-dev <abdoudu78130@gmail.com>
2025-02-07 18:20:28 +0100

    🔥

--- lua/adia-dev/plugins/oil.lua
+++ lua/adia-dev/plugins/oil.lua
@@ -1,34 +1,62 @@
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {},
-	event = "VimEnter",
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	keys = {
-		{ "<leader>e", "<CMD>Oil --float<CR>", desc = "Oil that thang" },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		event = "VimEnter",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		keys = {
+			{ "<leader>e", "<CMD>Oil --float<CR>", desc = "Oil that thang" },
+		},
+		config = function()
+			require("oil").setup({
+				keymaps = {
+					["g?"] = { "actions.show_help", mode = "n" },
+					["<CR>"] = "actions.select",
+					["<C-s>"] = { "actions.select", opts = { vertical = true } },
+					["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+					["<C-t>"] = { "actions.select", opts = { tab = true } },
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = { "actions.close", mode = "n" },
+					["<C-l>"] = "actions.refresh",
+					["-"] = { "actions.parent", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["`"] = { "actions.cd", mode = "n" },
+					["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+					["gs"] = { "actions.change_sort", mode = "n" },
+					["gy"] = "actions.yank_entry",
+					["gx"] = "actions.open_external",
+					["g."] = { "actions.toggle_hidden", mode = "n" },
+					["g\\"] = { "actions.toggle_trash", mode = "n" },
+				},
+				git = {
+					-- Return true to automatically git add/mv/rm files
+					add = function(path)
+						return true
+					end,
+					mv = function(src_path, dest_path)
+						return false
+					end,
+					rm = function(path)
+						return false
+					end,
+				},
+				-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+				skip_confirm_for_simple_edits = true,
+				win_options = {
+					signcolumn = "yes:2",
+				},
+			})
+		end,
 	},
-	config = function()
-		require("oil").setup({
-			keymaps = {
-				["g?"] = { "actions.show_help", mode = "n" },
-				["<CR>"] = "actions.select",
-				["<C-s>"] = { "actions.select", opts = { vertical = true } },
-				["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-				["<C-t>"] = { "actions.select", opts = { tab = true } },
-				["<C-p>"] = "actions.preview",
-				["<C-c>"] = { "actions.close", mode = "n" },
-				["<C-l>"] = "actions.refresh",
-				["-"] = { "actions.parent", mode = "n" },
-				["_"] = { "actions.open_cwd", mode = "n" },
-				["`"] = { "actions.cd", mode = "n" },
-				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-				["gs"] = { "actions.change_sort", mode = "n" },
-				["gy"] = "actions.yank_entry",
-				["gx"] = "actions.open_external",
-				["g."] = { "actions.toggle_hidden", mode = "n" },
-				["g\\"] = { "actions.toggle_trash", mode = "n" },
-			},
-		})
-	end,
+	{
+		"refractalize/oil-git-status.nvim",
+
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+
+		config = true,
+	},
 }
